dce00fb1ef89310cc94723068f93c03090ec65d0
Wyatt Sushinsky <wyattsushi@gmail.com>
2021-04-23 15:50:14 -0700

    revised server.js for independent work
    Signed-off-by: Wyatt Sushinsky <wyattsushi@gmail.com>

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/wonVentures', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => { 
+        console.log("Mongo connection open!")
+    })
+    .catch(err => {
+        console.log("Error!")
+        console.log(err)
+    })
+
 module.exports = {
     stocks: require("stocks.js"),
     user: require("user.js")
--- server.js
+++ server.js
@@ -1,48 +1,20 @@
 const express = require('express')
 const app = express(); 
-const path = require('path'); 
-const bcrypt = require('bcrypt'); 
 const methodOverride = require('method-override');
 
-app.use(express.urlencoded ({extended: true}))
-app.use(express.json())
-app.use(methodOverride('_method'))
-
-
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/wonVentures', {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => { 
-        console.log("Mongo connection open!")
-    })
-    .catch(err => {
-        console.log("Error!")
-        console.log(err)
-    })
-
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded ({extended: true}));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
 
 // Index route 
 app.get('/', (req,res) => {
-    res.render('./user/login.ejs')
+    res.render('index')
 })
-
-
 
 app.listen(3000, (req,res) => {
     console.log("Is this thing on?")
 });
-
-// index route 
-// new route 
-// show route 
-// update route 
-// override 
-// delete route 
-
-
-
-// login 
-// register 
-// show 
