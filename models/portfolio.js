c465fb3aec928631a5c3e1a57c9fbcc708a3d835
jonathancode <parkj97@gmail.com>
2021-04-25 14:57:00 -0700

    added dependencies on server.js

--- models/portfolio.js
+++ models/portfolio.js
@@ -1,6 +1,5 @@
 const portfolioSchema = new mongoose.Schema({
     stock: [{
-        id: ,          // need to update 
         name: String, 
         pricePerShare: Number,
         amountOwned: Number, 
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+// Dependencies 
 const express = require('express')
 const app = express(); 
 const methodOverride = require('method-override');
 
+// Dependency - ejs files 
 app.set('view engine', 'ejs');
 
+// Dependency - Models
+const models = require('./models')
+// Dependency - Controllers
+const controllers = require('./controllers')
+
+
+// utilize dependencies 
 app.use(express.urlencoded ({extended: true}));
 app.use(express.json());
 app.use(methodOverride('_method'));
