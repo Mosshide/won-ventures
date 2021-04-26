69b2c98a3fa0152c5a95e4b180534d16db7eaca0
jonathancode <parkj97@gmail.com>
2021-04-26 10:45:20 -0700

    made seeds.js to input some data.

--- models/stocks.js
+++ models/stocks.js
@@ -19,27 +19,10 @@ const stockSchema = new mongoose.Schema({
     },
     category:{
         type: String,
-        enum: ['Financial Services', 'Healthcare', 'Technology']  //sample industries  
+        enum: ['Financial Services', 'Healthcare', 'Technology', 'Consumer Goods']  //sample industries  
      },
 })
 
 // mongoose takes first param and creates a collection named Stocks. 
 const Stock = mongoose.model('Stock', stockSchema);
-
-// Add single instance to mongoDB 
-    // const amd = new Stock({name: 'AMD', price: 82.04, category: 'Technology'})
-    // Nothing in mongoDB at this point -> to save we must do amd.save() 
-
-// // insert many instances - sample stocks added to DB 
-Stock.insertMany([
-    {name: 'AMD', price: 82.04, category: 'Technology'},
-    {name: 'MU', price: 85.14, category: 'Technology'},
-    {name: 'JPM', price: 151.12, category: 'Financial Services'},
-    {name: 'JNJ', price: 165.52, category: 'Healthcare'},
-    {name: 'PFE', price: 38.66, category: 'Healthcare'},
-    {name: 'WFC', price: 44, category: 'Financial Services'},
-])
-    .then(securities=>{
-        console.log("Stocks inputted to database")
-        console.log(securities);
-    })
+module.exports = Stock 
