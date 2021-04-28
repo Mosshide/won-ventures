c8ae5548169f89e34dbb11fe31d0b8a093c6b9e7
jonathancode <parkj97@gmail.com>
2021-04-28 07:03:49 -0700

    skdl

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-// Dependencies 
+// packages required
 const express = require('express')
-const app = express(); 
+const app = express();
+const methodOverride = require('method-override')
 const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-
-// Dependency - ejs files 
-app.set('view engine', 'ejs');
-
-// Dependency - Models
-const models = require('./models')
-// Dependency - Controllers
-const controllers = require('./controllers');
-const Portfolio = require('./models/portfolio');
-
-// connect mongoose -> !TODO replace with cluster 
-mongoose.connect('mongodb://localhost:27017/wonVentures', {useNewUrlParser: true, useUnifiedTopology: true})
+// models required
+const Portfolio = require('./models/portfolio')
+const Stock = require('./models/stock')
+const User = require('./models/user')
+const Watchlist = require('./models/watchlist')
+// connect mongoose 
+mongoose.connect('mongodb://localhost:27017/solodb', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
     .then(() => {
         console.log("Connection successful")
     })
@@ -23,36 +22,63 @@ mongoose.connect('mongodb://localhost:27017/wonVentures', {useNewUrlParser: true
         console.log(err)
     })
 
+// Middleware Parse body and form data in POST request body 
+app.use(express.urlencoded({extended:true}))
 
-// utilize dependencies 
-app.use(express.urlencoded ({extended: true}));
-app.use(express.json());
-app.use(methodOverride('_method'));
-app.use(express.static('public'));
+// Middleware Put/patch/delete requests 
+app.use(methodOverride('_method'))
 
+// view ejs files in models 
+app.set('view engine', 'ejs')
 
+// routes - STOCKS 
+    // Index: Made a route for all available stocks 
+    app.get('/stock', async(req,res) => {
+        const stocks = await Stock.find({});
+        res.render('stock/index', {stocks})
+    })
 
-// ROOT route 
+    // Show: route for stock by ID 
+    app.get('/stock/:id', async(req,res) => {
+        const stocks = await Stock.findById(req.params.id)
+        res.render('stock/show', {stocks})
+    })
+
+    // Edit: route for buying/selling
+    // buy
+    app.get('/stock/:id/buy', async(req,res) =>{
+        const stocks = await Stock.findById(req.params.id)
+        if(Portfolio.stock !== stocks){
+            Portfolio.insert(stocks);
+            Portfolio.save();
+        } else {}
+        res.render('stock/buy', {stocks}) 
+    })
+    // sell 
+    app.get('/stock/:id/sell', async(req,res) =>{
+        const stocks = await Stock.findById(req.params.id)
+        const portfolios = Portfolio.
+        res.render('stock/sell', {stocks}) 
+    })
+
+// Root Route - Load Home page -> Log in 
 app.get('/', (req,res) => {
-    res.render('/user/login')
+    res.render('home')
 })
 
-// // Index Route
-app.get('/myaccount', async (req,res) => {
-    const myPortfolio = await Portfolio.find({});
-    res.render('/myaccount/index')
+// User Routes - wyatt 
+// Index
+    // render multiple of a resource 
+app.get('/user', (req,res) => {
+    res.render('user/index')
 })
 
-// Create 
-
-// Read 
-
-// Update
-
-// Delete
-
+// Create - Add positions to portfolio
+app.get('/user/new', (req,res) => {
+    res.render('user/new')
+})
 
 
 app.listen(3000, (req,res) => {
-    console.log("Is this thing on?")
-});
+    console.log("Listening on Port 3000!")
+})
