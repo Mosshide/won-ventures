2b92e58ffa49a946cc055ec7d4702ad70cea13b6
jonathancode <parkj97@gmail.com>
2021-04-28 09:43:08 -0700

    removed stocks.js controller

--- controllers/stocks.js
+++ controllers/stocks.js
@@ -1,17 +1,1 @@
-// const express = require('express');
-// const app = express();
-// const mongoose = require('mongoose')
-// const stocks = require('../models/stocks.js');
-
-// app.get('/stocks', async(req,res) =>{
-//     const stocks = await stocks.find({})
-//     console.log(stocks);
-//     res.render('products/index', {stocks})
-// })
-
-
-
-
-
-
-// module.exports = Router();
+const express = require('express')
